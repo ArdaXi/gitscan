f9681f049eb5002e539b09bb8fcd04d18dcf800b
Holthuizen <LK01PD@europe.intranet>
2017-06-08 12:54:46 +0200

    first ws

--- server.go
+++ server.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/ardaxi/gitscan/database"
 	"github.com/ardaxi/gitscan/providers"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 var db *database.DB
 var baseURL string
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+var homeTempl = template.Must(template.New("").Parse(homeHTML))
+
+const pongWait = 60 * time.Second
 
 func serve(mydb *database.DB, mybaseurl string) {
 	db = mydb
 	baseURL = mybaseurl
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/ws", wsHandler).Methods("GET").Queries("token", "{token}")
 	r.HandleFunc("/projects/index", indexHandler).Methods("GET").Queries("token", "{token}")
 	r.HandleFunc("/projects/{id:[0-9]+}.html", projectHandler).Methods("GET").Queries("token", "{token}")
 	log.Println("Starting server...")
@@ -26,10 +39,80 @@ func serve(mydb *database.DB, mybaseurl string) {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`<html><head><title>GitScan</title></head><body>
-	<form action="/projects/index" method="get"><label for="token">Token:</token>
-	<input type="text" id="token" name="token"><button type="submit">Login</button>
-	</form></body></html>`))
+	var v = struct {
+		Host string
+	}{
+		Host: r.Host,
+	}
+	if err := homeTempl.Execute(w, &v); err != nil {
+		log.Printf("Failed to execute template: %s", err)
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Couldn't start ws session: %s", err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	token := vars["token"]
+
+	go writer(ws, token)
+	reader(ws)
+}
+
+func reader(ws *websocket.Conn) {
+	defer ws.Close()
+	ws.SetReadLimit(512)
+	ws.SetReadDeadline(time.Now().Add(pongWait))
+	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, _, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+	}
+}
+
+func writer(ws *websocket.Conn, token string) {
+	provider, err := providers.Providers["gitlab"](&providers.Options{
+		Token: token,
+		URL:   baseURL,
+	})
+	if err != nil {
+		log.Printf("Error occurred logging into Gitlab: %s", err)
+		if err := ws.WriteMessage(websocket.TextMessage, status("Failed to login")); err != nil {
+			log.Printf("Error occurred writing error: %s", err)
+		}
+		return
+	}
+	if err := ws.WriteMessage(websocket.TextMessage, status("Logged in as %s", provider.Username())); err != nil {
+		log.Printf("Error occurred writing error: %s", err)
+	}
+
+	projects := provider.ListAllProjects()
+	for project := range projects {
+		count, _ := db.ResultCount(project.ID())
+		if count == 0 {
+			continue
+		}
+
+		res := result(`<a href="/projects/%d.html?token=%s">%s</a> (%d results)`, project.ID(), token, project.Name(), count)
+		if err := ws.WriteMessage(websocket.TextMessage, res); err != nil {
+			log.Printf("Error occurred writing result: %s", err)
+		}
+	}
+}
+
+func status(msg string, a ...interface{}) []byte {
+	return []byte(fmt.Sprintf(`{"html":"`+msg+`"}`, a...))
+}
+
+func result(msg string, a ...interface{}) []byte {
+	safeMsg, _ := json.Marshal(msg)
+	return []byte(fmt.Sprintf(`{"html":`+string(safeMsg)+`,"result":true}`, a...))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -122,3 +205,50 @@ func flush(w http.ResponseWriter) {
 		flusher.Flush()
 	}
 }
+
+type wsMessage struct {
+	HTML   string `json:"html"`
+	Result bool   `json:"result"`
+}
+
+const homeHTML = `<!DOCTYPE html><html lang="en"><head>
+<title>GitScan Report</title></head><body>
+<form id="login-form"><label for="token">Token:</label>
+<input type="text" id="token" name="token"><button type="submit">Login</button>
+</form><br><div id="status"><div>Not logged in</div><br></div><br><div id="results"></div>
+<script type="text/javascript">
+    var addResult = function(results, data) {
+		var result = document.createElement('div')
+		result.innerHTML = data.html
+		results.appendChild(result)
+	}
+    var connect = function(token) {
+		var status = document.getElementById('status')
+		var results = document.getElementById('results')
+		var conn = new WebSocket("ws://{{.Host}}/ws?token="+token)
+		conn.onclose = function(e) {
+			newStatus = document.createElement('div')
+			newStatus.textContent = "Connection closed"
+			status.appendChild(newStatus)
+		}
+		conn.onmessage = function(e) {
+			var data = JSON.parse(e.data)
+			if (data.result) {
+				addResult(results, data)
+				return
+			}
+			newStatus = document.createElement('div')
+			newStatus.textContent = data.html
+			status.appendChild(newStatus)
+		}
+	}
+    window.onload=function() {
+		document.getElementById("login-form").onsubmit=function() {
+			var tokenField = document.getElementById("token")
+			connect(tokenField.value)
+			return false
+		}
+	}
+</script>
+</body></html>
+`
